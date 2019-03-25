81583de1050ea5409fffd26973ae91f6cb410fa8
taraldjoh <tarald.johansen@outlook.com>
2019-03-25 21:14:41 +0100

    added post request

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const http = new easyHTTP();
 
 // GET Post
-http.get("https://jsonplaceholder.typicode.com/posts", function(err, posts) {
+// http.get("https://jsonplaceholder.typicode.com/posts", function(err, posts) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// Create Data
+const data = {
+  title: "Custom Post",
+  body: "This is a custom post"
+};
+
+// Create POST
+http.post("https://jsonplaceholder.typicode.com/posts", data, function(
+  err,
+  post
+) {
   if (err) {
     console.log(err);
   } else {
-    console.log(posts);
+    console.log(post);
   }
 });
--- easyhttp.js
+++ easyhttp.js
@@ -20,6 +20,18 @@ easyHTTP.prototype.get = function(url, callback) {
 };
 
 // Make HTTP POST Request
+easyHTTP.prototype.post = function(url, data, callback) {
+  this.http.open("POST", url, true);
+  this.http.setRequestHeader("Content-type", "application/json");
+
+  let self = this;
+
+  this.http.onload = function() {
+    callback(null, self.http.responseText);
+  };
+
+  this.http.send(JSON.stringify(data));
+};
 
 // Make HTTP PUT Request
 
